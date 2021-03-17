4c8a7175b802d2a9565764cda109df1aadd6ba97
Admin <Admin@Admin>
2021-03-17 19:24:02 +0700

    update Topic_01 and add new Topic 02 exercise

--- src/selenium/Topic_01_Locator_Selenium_Exercises.java
+++ src/selenium/Topic_01_Locator_Selenium_Exercises.java
@@ -2,6 +2,7 @@ package selenium;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,6 +14,10 @@ import org.testng.annotations.AfterClass;
 
 public class Topic_01_Locator_Selenium_Exercises {
 	WebDriver driver;
+	By emailTextboxBy = By.id("email");
+	By passwordTextboxBy = By.id("pass");
+	By loginButtonBy = By.id("send2");
+	
   @BeforeClass
   public void beforeClass() {
 	  System.setProperty("webdriver.chrome.driver", ".\\libraries\\chromedriver.exe");
@@ -22,60 +27,68 @@ public class Topic_01_Locator_Selenium_Exercises {
 		
   }
   
+  // Run for each test case
+  @BeforeMethod
+  public void beforeMethod() {
+	  driver.get("http://live.guru99.com/index.php/customer/account/login/");
+  }
+  
   //TC_01_Login Email Password Empty
   @Test
   public void TC_01_Login_Empty() throws Exception {
-	  driver.get("http://live.guru99.com/index.php/customer/account/login/");
 	  
-	  driver.findElement(By.id("email")).sendKeys("");
-	  driver.findElement(By.id("pass")).sendKeys("");
+	  driver.findElement(emailTextboxBy).sendKeys("");
+	  driver.findElement(passwordTextboxBy).sendKeys("");
 	  Thread.sleep(1000);
-	  driver.findElement(By.id("send2")).click();
+	  driver.findElement(loginButtonBy).click();
 	  Thread.sleep(1000); 
-	  Assert.assertEquals(driver.findElement(By.id("advice-required-entry-email")).getText(), "This is a required field.");
-	  Assert.assertEquals(driver.findElement(By.id("advice-required-entry-pass")).getText(), "This is a required field.");
+	  String emailErrorMessage = driver.findElement(By.id("advice-required-entry-email")).getText();
+	  String passwordErrorMessage = driver.findElement(By.id("advice-required-entry-pass")).getText();
+	  
+	  // Method to Verify data
+	  Assert.assertEquals(emailErrorMessage, "This is a required field.");
+	  Assert.assertEquals(passwordErrorMessage, "This is a required field.");
   }
   
   
   //TC_02_Login Email invalid
   @Test
   public void TC_02_Login_Invalid_Username() throws Exception {
-	  driver.get("http://live.guru99.com/index.php/customer/account/login/");
 	  
-	  driver.findElement(By.id("email")).sendKeys("123124@412412.34534");
-	  //driver.findElement(By.id("pass")).sendKeys("");
+	  driver.findElement(emailTextboxBy).sendKeys("123124@412412.34534");
 	  Thread.sleep(1000);
-	  driver.findElement(By.id("send2")).click();
+	  driver.findElement(loginButtonBy).click();
 	  Thread.sleep(1000);
-	  Assert.assertEquals(driver.findElement(By.id("advice-validate-email-email")).getText(), "Please enter a valid email address. For example johndoe@domain.com.");
+	  String emailInvalidErrorMessage = driver.findElement(By.id("advice-validate-email-email")).getText();
+	  Assert.assertEquals(emailInvalidErrorMessage, "Please enter a valid email address. For example johndoe@domain.com.");
   }
   
   
   //TC_03_Login Password < 6 characters
   @Test
   public void TC_03_Login_Password_Less_Than_Six_Characters() throws Exception {
-	  driver.get("http://live.guru99.com/index.php/customer/account/login/");
 	  
-	  driver.findElement(By.id("email")).sendKeys("nguyenming1012@gmail.com");
-	  driver.findElement(By.id("pass")).sendKeys("123");
+	  driver.findElement(emailTextboxBy).sendKeys("nguyenming1012@gmail.com");
+	  driver.findElement(passwordTextboxBy).sendKeys("123");
 	  Thread.sleep(1000);
-	  driver.findElement(By.id("send2")).click();
+	  driver.findElement(loginButtonBy).click();
 	  Thread.sleep(1000);
-	  Assert.assertEquals(driver.findElement(By.id("advice-validate-password-pass")).getText(), "Please enter 6 or more characters without leading or trailing spaces.");
+	  String passwordInvalidErrorMessage = driver.findElement(By.id("advice-validate-password-pass")).getText();
+	  Assert.assertEquals(passwordInvalidErrorMessage, "Please enter 6 or more characters without leading or trailing spaces.");
   }
   
   
   //TC_04_Login Password Invalid 
   @Test
   public void TC_04_Login_Password_Invalid() throws Exception {
-	  driver.get("http://live.guru99.com/index.php/customer/account/login/");
 	  
-	  driver.findElement(By.id("email")).sendKeys("nguyenming1012@gmail.com");
-	  driver.findElement(By.id("pass")).sendKeys("23423415125");
+	  driver.findElement(emailTextboxBy).sendKeys("nguyenming1012@gmail.com");
+	  driver.findElement(passwordTextboxBy).sendKeys("23423415125");
 	  Thread.sleep(1000);
-	  driver.findElement(By.id("send2")).click();
+	  driver.findElement(loginButtonBy).click();
 	  Thread.sleep(2000);
-	  Assert.assertEquals(driver.findElement(By.xpath("//li[@class='error-msg']//span")).getText(), "Invalid login or password.");
+	  String notificationErrorMessage = driver.findElement(By.xpath("//li[@class='error-msg']//span")).getText();
+	  Assert.assertEquals(notificationErrorMessage, "Invalid login or password.");
   }
   @AfterClass
   public void afterClass() {
---
+++ src/selenium/Topic_02_Webbrowser_Api_Commands_Exercises.java
@@ -0,0 +1,118 @@
+package selenium;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Topic_02_Webbrowser_Api_Commands_Exercises {
+	WebDriver driver;
+	By myAccountLink = By.xpath("(//a[@title=\"My Account\"])[2]");
+	By createAnAccountButton = By.xpath("//div[@class=\"buttons-set\"]//a");
+	
+	
+	@BeforeClass	
+  public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", ".\\libraries\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+  }
+	
+	  @BeforeMethod
+	  public void beforeMethod() {
+		  driver.get("http://live.guru99.com");
+	  }
+	
+  
+  @Test
+  public void TC_01_Verify_Url() throws Exception {
+	  
+	  driver.findElement(myAccountLink).click();
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  //get Login Page URL
+	  String loginPage = driver.getCurrentUrl();
+	  System.out.println(loginPage);
+	  Assert.assertEquals(loginPage,"http://live.demoguru99.com/index.php/customer/account/login/");
+	  driver.findElement(createAnAccountButton).click();
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  //get Register Page URL
+	  String registerPage = driver.getCurrentUrl();
+	  System.out.println(registerPage);
+	  Assert.assertEquals(registerPage,"http://live.demoguru99.com/index.php/customer/account/create/");
+	  
+	  
+  }
+  
+  @Test
+  public void TC_02_Verifiy_Title() {
+	  
+	  driver.findElement(myAccountLink).click();
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  //get TITLE Login Page
+	  String titleLoginPage = driver.getTitle();
+	  System.out.println("LOGIN PAGE TITLE: "+ titleLoginPage);
+	  Assert.assertEquals(titleLoginPage, "Customer Login");
+	  driver.findElement(createAnAccountButton).click();
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  //get TITLE Register Page 
+	  String titleRegisterPage = driver.getTitle();
+	  System.out.println("REGISTER PAGE TITLE: "+ titleRegisterPage);
+	  Assert.assertEquals(titleRegisterPage,"Create New Customer Account");
+	  
+  }
+  
+  @Test
+  public void TC_03_Navigate_Function() {
+	  
+	  driver.findElement(myAccountLink).click();
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.findElement(createAnAccountButton).click();
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  //get Register Page URL
+	  String registerPage = driver.getCurrentUrl();
+	  System.out.println(registerPage);
+	  Assert.assertEquals(registerPage,"http://live.demoguru99.com/index.php/customer/account/create/");
+	  // BACK TO LOGIN PAGE AND GET URL
+	  driver.navigate().back();
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  String loginPage = driver.getCurrentUrl();
+	  System.out.println(loginPage);
+	  Assert.assertEquals(loginPage,"http://live.demoguru99.com/index.php/customer/account/login/");
+	  // FORWARD TO REGISTER PAGE AGAIN AND VERIFY TITLE
+	  driver.navigate().forward();
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  String titleRegisterPage = driver.getTitle();
+	  System.out.println("REGISTER PAGE TITLE: "+ titleRegisterPage);
+	  Assert.assertEquals(titleRegisterPage,"Create New Customer Account");
+  }
+  
+  @Test
+  public void TC_04_Get_Page_Resource() {
+	  
+	  driver.findElement(myAccountLink).click();
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  // Get All Resource of LOGIN PAGE
+	  String loginPageResource = driver.getPageSource();
+	  Assert.assertTrue(loginPageResource.contains("Login or Create an Account"));
+	  driver.findElement(createAnAccountButton).click();
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  // Get All Resource of REGISTER PAGE
+	  String registerPageResource = driver.getPageSource();
+	  Assert.assertTrue(registerPageResource.contains("Create an Account"));
+	  
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.close();
+  }
+
+}
